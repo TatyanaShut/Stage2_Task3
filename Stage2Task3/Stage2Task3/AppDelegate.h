7be9c54b2531d5701b196c2fbb8ced6dafa84d6d
Tatyana Shut <tatyanashut99@gmail.com>
2019-06-16 14:42:00 +0300

    init

---
+++ Stage2Task3/Stage2Task3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stage2Task3
+//
+//  Created by Tatyana Shut on 16.06.2019.
+//  Copyright © 2019 Tatyana Shut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stage2Task3/Stage2Task3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stage2Task3
+//
+//  Created by Tatyana Shut on 16.06.2019.
+//  Copyright © 2019 Tatyana Shut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
